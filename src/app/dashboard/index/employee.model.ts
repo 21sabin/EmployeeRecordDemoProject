ac8763abb0aa76134b462f8659aa70084445523e
Saigal <saigal.amatya@gmail.com>
2018-02-18 16:29:30 +0545

    modified model class

--- src/app/dashboard/index/employee.model.ts
+++ src/app/dashboard/index/employee.model.ts
@@ -14,10 +14,22 @@ export class Employees{
 
         // }
 
-        constructor(private email:string,private password:string,private username:string,private employeeType:string){
+        firstName:string;
+        lastName:string;
+        username:string;
+        email:string;
+        password:string;
+        employeeType:string;
+        
+
+        constructor(firstName:string,lastName:string,
+                    username:string,email:string,
+                    password:string,employeeType:string){
             this.email=email;
             this.password=password;
             this.username=username;
+            this.firstName=firstName;
+            this.lastName=lastName;
             this.employeeType=employeeType;
         }
 }
