cd94ff71ab29407c62a243e8478803d5e7bf1843
Saigal <saigal.amatya@gmail.com>
2018-02-18 16:34:53 +0545

    employee service

--- src/app/dashboard/index/employee.service.ts
+++ src/app/dashboard/index/employee.service.ts
@@ -1,3 +1,4 @@
+
 import {Injectable} from '@angular/core';
 import {Http,Response,Headers} from '@angular/http';
 import {Observable} from 'rxjs/Rx';
@@ -5,34 +6,34 @@ import {Employees} from './employee.model'
 
 @Injectable()
 export class EmployeeService{
-
+    
     constructor(private http:Http){}
 
     addEmployee(emp:Employees){
-        const employee=JSON.stringify(emp);
-        let headers = new Headers({'Content-Type':'application/json'});
+        const employee=JSON.stringify(emp);''
+        let headers=new Headers({'Content-Type':'application/json'});
         return this.http.post("http://localhost:3000/Employees",employee,{headers:headers});
     }
 
     getEmployeeList(){
         return this.http.get("http://localhost:3000/Employees")
         .map((res:Response)=>{
-            const employees = res.json();
-            let transformEmployee: Employees[]=[];
-            console.log(employees)
+            const employees=res.json();
+            let transformEmployee:Employees[]=[];
+            console.log(employees);
 
             for(let emp of employees){
-                transformEmployee.push(new Employees(emp.email, emp.password, emp.username, emp.employeeType));
+                transformEmployee.push(new Employees(emp.firstName,emp.lastName,emp.username,emp.email,emp.password,emp.employeeType));
             }
-            console.log(transformEmployee, "tranfromed data")
+            console.log(transformEmployee,"tranformed dta")
             return transformEmployee;
 
         })
-        .catch((error:Response) => {
-
+        .catch((error:Response)=>{
+            
             return Observable.throw(error.json())
         })
     }
 
-
+    
 }
