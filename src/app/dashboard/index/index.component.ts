b2d61740449e632bf6d45880b7939226d2a723b2
Saigal <saigal.amatya@gmail.com>
2018-02-18 16:27:26 +0545

    added search feature

--- src/app/dashboard/index/index.component.ts
+++ src/app/dashboard/index/index.component.ts
@@ -13,17 +13,39 @@ export class IndexComponent implements OnInit {
 
   constructor(private employeeService:EmployeeService) { }
 
-  employees: Employees[];
+  employees:Employees[];
+  clonedEmployees: Employees[] = [];
+  searchedEmployees: Array<any> = [];
+
+  searchClick = false;
 
   ngOnInit() {
      this.employeeService.getEmployeeList()
      .subscribe(
       (employee:Employees[])=>{
           this.employees = employee;
+          console.log("asdsad", this.employees);
+          
       }
      )
+  }
 
-    // console.log("asdasd4");
+  searchEmployee(searchName) {
+
+    this.searchedEmployees = [];
+    this.searchClick = true;
+    console.log("cloned employees", this.clonedEmployees);
+    console.log("employees", this.employees);
+    for(var i = 0; i < this.employees.length; i++) {
+      
+      if(this.employees[i].firstName.toLowerCase().startsWith(searchName.toLowerCase())) {
+        this.searchedEmployees.push(this.employees[i]);
+        console.log(this.searchedEmployees[i]);
+        
+      }
+      console.log(searchName);
+    }
+    this.employees = this.searchedEmployees;
 
   }
 
